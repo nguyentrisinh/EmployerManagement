9568a477341e5ddcde8a2ae7966ca670b187c142
Sinhnt <nguyentrisinh0810@gmail.com>
2017-10-20 11:10:28 +0700

    FullTime Salary without test

---
+++ EmployerManagement/FullTimeSalary.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "iostream"
+#include "stdio.h"
+#include "Salary.h"
+#include "FulltimeEmployer.h"
+
+
+class FullTimeSalary :
+	public Salary 
+{
+	FullTimeSalary();
+	~FullTimeSalary();
+
+	virtual void TinhLuong(int thang, int nam, Employer* employer);
+	virtual void XuatLuong();
+};
