b9ec1babd411555c494777b685563c8f25830ca1
Umino <umino.sharp@gmail.com>
2017-10-20 11:29:59 +0700

    Auto stash before merge of "master" and "origin/master"
    merge

--- EmployerManagement/EmployerManagement.cpp
+++ EmployerManagement/EmployerManagement.cpp
@@ -8,9 +8,17 @@
 int main()
 {
 	Manage* manage = new Manage();
-	manage->NhapDepartment();
-	manage->NhapNhanVien();
+	//manage->NhapDepartment();
+	//manage->NhapNhanVien();
+	//system("cls");
+	manage->CreateDummyData();
 	manage->XuatNhanVien();
+	cout << "---------------------------------" << endl;
+
+
+	/*manage->XoaNhanVien();
+	system("cls");
+	manage->XuatNhanVien();*/
 	system("pause");
     return 0;
 }
